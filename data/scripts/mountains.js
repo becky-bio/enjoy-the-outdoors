691b4a8b30b76cd06f2a260411f831fe2d606374
becky-bio <beckybio07@gmail.com>
2023-11-16 10:07:50 -0500

    changes made to the mountain page

--- data/scripts/mountains.js
+++ data/scripts/mountains.js
@@ -1,8 +1,8 @@
 "use strict"
-let selection = document.querySelector ('ul')
-let result = document.querySelector('h4')
 
-selection.addEventListener('change', ()=> {
-    result.innerText = selection.dropdownItem[selection.selectedIndex].text;
+mountainData.onchange = function displayData() {
+    result.innerText = this.value
 }
-)
+
+
+
